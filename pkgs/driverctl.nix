a97ee7f1398d5d9c00fad4eea4c5b788f0972d6c
Ilya Meignan--Masson <ilya.meignan-masson@mailoo.org>
2025-04-28 06:42:17 +0000

    fixing modprobe

--- pkgs/driverctl.nix
+++ pkgs/driverctl.nix
@@ -7,11 +7,12 @@ pkgs.stdenv.mkDerivation {
 			url = "https://gitlab.com/driverctl/driverctl.git";
 	    rev = "3a97f2d0902c8c8d3b059cc33edf275e9fc98aee";
 			hash = "sha256-H8aahtsmPwlqxqfgvrvaKQznOnnvmfQ370y+yeleNeU=";
-	  };
+		};
 
 	  installPhase = ''
 	    mkdir -p $out/bin
-	    cp driverctl $out/bin/driverctl
+			cp driverctl $out/bin/driverctl
+			sed -i 's/\/sbin\/modprobe/modprobe/g' $out/bin/driverctl
 	  '';
 
 	  meta = with pkgs.lib; {
